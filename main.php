674a670a492945ea343574ffd592f5c2ef6cdb82
jeff <jepoyoy@gmail.com>
2019-06-17 18:52:53 +0800

    updated title in main.php

--- main.php
+++ main.php
@@ -13,7 +13,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <link rel="icon" href="loan.png">
-    <title>Loan Now </title>
+    <title>Seph Project</title>
 
 <style>
 .jumbotron{
